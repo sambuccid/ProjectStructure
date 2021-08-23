67d52e15fb7f59a88b6b9b3cd15852ac20df15d1
davidsambucci <davidsambucci@pop-os.localdomain>
2021-08-23 16:58:44 +0100

    Initial Commit

---
+++ src/main/java/com/sambccd/ProjectStructure/App.java
@@ -0,0 +1,13 @@
+package com.sambccd.ProjectStructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
